9a2e87731092cad5599a95559749913f18e7ec46
VIPINRAJK26 <vipinrajk026@gmail.com>
2026-08-31 23:44:50 +0530

    added events CRUD and pagination

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import EventDetailView, EventListView,EventCreateView
+from .views import EventDetailView, EventListView,EventCreateView,EventDeleteView,EventUpdateView
 
 
 urlpatterns = [
     path("", EventListView.as_view(), name="event-list"),
     path("create/", EventCreateView.as_view(), name="event-create"),
     path("<int:pk>/", EventDetailView.as_view(), name="event-detail"),
+    path("<int:pk>/update/", EventUpdateView.as_view(), name="event-update"),
+    path("<int:pk>/delete/", EventDeleteView.as_view(), name="event-delete"),
 ]
+
+
--- apps/events/views.py
+++ apps/events/views.py
@@ -36,7 +36,7 @@ class EventListView(generics.ListAPIView):
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        queryset = Event.objects.all()
+        queryset = Event.objects.order_by("start_time")
 
         return EventFilter.filter_queryset(
             queryset=queryset,
@@ -48,3 +48,48 @@ class EventDetailView(generics.RetrieveAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [AllowAny]
+
+
+class EventUpdateView(generics.GenericAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsStaffUser]
+    queryset = Event.objects.all()
+
+    def patch(self, request, pk):
+        event = self.get_object()
+
+        serializer = self.get_serializer(
+            event,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+
+        event = EventService.update_event(
+            event=event,
+            **serializer.validated_data,
+        )
+
+        return Response(
+            {
+                "message": "Event updated successfully.",
+                "event": EventSerializer(event).data,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+
+class EventDeleteView(generics.GenericAPIView):
+    permission_classes = [IsStaffUser]
+    queryset = Event.objects.all()
+
+    def delete(self, request, pk):
+        event = self.get_object()
+        event.delete()
+
+        return Response(
+            {
+                "message": "Event deleted successfully."
+            },
+            status=status.HTTP_200_OK,
+        )
