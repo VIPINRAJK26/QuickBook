72f53ad142c7130cd5b81757943046526b5f1f70
VIPINRAJK26 <vipinrajk026@gmail.com>
2026-09-01 09:41:56 +0530

    added views and urls for dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from apps.accounts.models import User
+from apps.bookings.models import Booking
+from apps.events.models import Event
+from apps.vendors.models import Vendor
+
+
+class DashboardStatsView(APIView):
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        data = {
+            "total_customers": User.objects.filter(
+                is_staff=False
+            ).count(),
+
+            "total_vendors": Vendor.objects.count(),
+
+            "total_events": Event.objects.count(),
+
+            "total_bookings": Booking.objects.count(),
+        }
+
+        return Response(data)
